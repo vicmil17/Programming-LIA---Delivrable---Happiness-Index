401b5ffe50d91727dd36fdf6ec6f515d5d60a1c3
vicmil17 <victoria_mil_17@hotmail.com>
2025-10-09 13:37:00 -0400

    Update LIA - HAPPINESS INDEX - CODE - VO.py

--- LIA - HAPPINESS INDEX - CODE - VO.py
+++ LIA - HAPPINESS INDEX - CODE - VO.py
@@ -24,4 +24,5 @@ data = np.loadtxt("world-happiness-report-2021.csv", delimiter=",", skiprows=1, 
 
 # each column seperately
 ladder, gdp, support, life, freedom, generosity, corruption = data.T
-print("Ladder sample:", ladder[:5])
+print("Ladder sample:", ladder[:5]) 
+hi 
